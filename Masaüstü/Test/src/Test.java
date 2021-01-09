38b5ccd6e1dd5abc6a3c1a50880032b67c28f46e
OktayBora <iletisim.bora@gmail.com>
2021-01-09 17:13:38 +0300

    İlk Eclipse Denemesi

---
+++ Masaüstü/Test/src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		  int i;
+			System.out.println("Döngü başladı");
+			for (i=0; i<12; i++)
+			System.out.println("i= " + i);
+			System.out.println("Döngü Bitti");
+			
+			
+				
+				  int j;
+					System.out.println("Döngü başladı");
+					for (j=0; j<5; j++)
+					System.out.println("j= " + j);
+					System.out.println("Döngü Bitti");
+					
+			
+	}
+}
